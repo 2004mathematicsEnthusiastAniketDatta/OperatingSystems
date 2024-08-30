a8c60f244d662881fec221bafbd8c0c2bcd4066a
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-08-30 19:42:26 +0000

    arrays

--- shell_scripting/12_basics.sh
+++ shell_scripting/12_basics.sh
@@ -8,3 +8,23 @@ echo "value in 1st index: ${myArray[1]}"
 echo "value in 2nd index: ${myArray[2]}"
 echo "value in 3rd index: ${myArray[3]}"
 echo "all the elements in Array: ${myArray[*]}"
+#Length of the array
+echo "Number of values of the array: ${#myArray[*]}" 
+#Array slicing 
+echo "Values from index 1 to index 3: ${myArray[*]:1:3}"
+#${Arrayname[*]:starting index:number of elements from the starting index to be printed}
+
+#updating an Array and concatenating more elements
+myArray+=( New 30 40 )
+
+echo "Updated Array:${myArray[*]}"
+
+#key-value pairs
+
+declare -A dictionary
+dictionary=( [name]=Aniket [age]=20 [city]=Kolkata )
+echo "Name is ${dictionary[name]}"
+echo "Age is ${dictionary[age]}"
+echo "City is ${dictionary[city]}"
+
+
