18fbafe739c39cc77f05fbbfebbdd0e173097e29
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-08-30 16:38:00 +0000

    bash

---
+++ shell_scripting/09_basics.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo -e "Enter the range till which prime numbers are to be printed"
+read r
+is_prime() {
+for ((i=0;i<=$1;i++));do
+     if [ i -le 1 ];then
+        return 1
+    fi
+    for((j=2;j*j<=i;j++));do
+     if [ $ ] 
+
+}
