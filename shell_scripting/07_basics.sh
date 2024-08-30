b38234be2a2d22e9229e58497a9168171916ae21
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-08-30 15:51:23 +0000

    factorial

--- shell_scripting/07_basics.sh
+++ shell_scripting/07_basics.sh
@@ -4,3 +4,11 @@ write a shell script to find the factorial value of any integer entered through 
 COMMENT
 echo -e "Enter the number"
 read n
+i=0
+f=1
+while [ $i -lt $n ];do
+  i=`expr $i + 1`
+  f=`expr $f \* $i`
+done
+
+echo "The factorial of $n is: $f" 
