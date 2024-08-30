ee4893995b1bee9d224fa51158dd6b3b3e4f20d3
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-08-30 13:59:42 +0000

    PRIME_BASH

--- shell_scripting/06_basics.sh
+++ shell_scripting/06_basics.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-<<COMMENT
-PRIME NUMBERS HAVE TWO FACTORS 1 AND THE SAME NUMBER , 
-THUS 1 IS UNITY WHICH IS NEITHER PRIME NOR COPRIME AND FOLLOWS THE RULES OF THE STRUCTURE UNITY , BECAUSE 1 HAS ONLY ONE  FACTOR.
-2 IS PRIME BY DEFINITION, 3 IS ALSO PRIME WITH TWO FACTORS 1 AND 3 . 4 IS NOT A PRIME NUMBER. 4 WITH THREE FACTORS 1,2,4 . 
-
-Here is a simple Bash script to check if a number is prime or not:
-Bash
-
-LET US EXPLORE FOR LOOPS IN SHELL SCRIPING WITH PRIME NUMBER PROGRAM
-
-COMMENT
-
-
-#!/bin/bash
 
 # Function to check if a number is prime
 is_prime() {
@@ -35,7 +21,7 @@ read num
 
 # Check if the number is prime
 if is_prime $num; then
-    echo "$num is not a prime number"
-else
     echo "$num is a prime number"
+else
+    echo "$num is not a prime number"
 fi
