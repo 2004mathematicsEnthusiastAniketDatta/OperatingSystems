974d990052ec18de5d6475a0963d12871f3a28a9
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-08-30 14:09:03 +0000

    BASH

--- shell_scripting/01_basic.sh
+++ shell_scripting/01_basic.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+<<COMMENT
+1. #!/bin/bash
+
+This is the shebang line, which tells the system which interpreter to use to run the script. In this case, it's specifying the Bash shell. The # symbol is a comment character in Bash, but the shebang line is an exception - it's treated as a special directive.
+
+When a script starts with a shebang line, the system will look for the specified interpreter (in this case, /bin/bash) and run the script using that interpreter. This line is only needed if you plan to run the script as an executable (i.e., ./script.sh); if you run it using bash script.sh, the shebang line is ignored.
+COMMENT
 
 echo "Hey buddy"
