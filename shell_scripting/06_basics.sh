bf87691c335118f88d1b49e87857a4064bf8cbce
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-08-30 14:14:37 +0000

    bash

--- shell_scripting/06_basics.sh
+++ shell_scripting/06_basics.sh
@@ -128,10 +128,10 @@ This closes the if statement.
 COMMENT
 # Function to check if a number is prime
 is_prime() {
-    if [ $1 -le 1 ]; then
+    if [ $1 -le 1 ]; then 
         return 1
     fi
-
+    # $1 refers to the first command-line argument passed to the function.
     for ((i=2; i*i<=$1; i++)); do
         if [ $(( $1 % i )) -eq 0 ]; then
             return 1
