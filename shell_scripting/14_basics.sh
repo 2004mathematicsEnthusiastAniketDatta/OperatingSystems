c02393885e00f36b52b0e014e4faf3e96ea3c4a5
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-08-31 05:32:07 +0000

    arrays user interaction

--- shell_scripting/14_basics.sh
+++ shell_scripting/14_basics.sh
@@ -2,7 +2,7 @@
 
 #user interaction
 
-#user input
+#user input for a variable
 
 read -p "Name:" name
 echo "User's name is $name"
@@ -11,3 +11,13 @@ echo "What is your age"
 read age
 echo "User 's age is $age"
 
+#user input for Arrays:
+
+echo "Enter array elements separated by spaces:"
+read -a myArray
+# myArray[@] takes individual array elements until space is noticed and this allows seperate printing of every word of the array with spaces among them , 
+#this goes on till the last element of the array
+echo "Array elements are:"
+for element in "${myArray[@]}"; do
+  echo "$element"
+done
