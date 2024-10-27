d69874ab81c0a5fe64afee3403643a9ae139a8c9
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-10-27 15:34:41 +0000

    file

--- shell_scripting/35_basic.sh
+++ shell_scripting/35_basic.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-# if value not provided to variable then emptu variable is there so empty space is printed on terminal when we try to get the echo
-#
+# if value not provided to variable then empty variable is there so empty space is printed on terminal when we try to get the echo
+
+#to access the arguements
+
+func(){
+    echo "first arguement is $1";
+    echo "second arguement is $2";
+    echo "All the arguements are - $@"; 
+    echo "Number of arguements are - $#";   
+}
+func 0 1 2
+
+#For loop to access the values from arguements
+
+for filename in $@
+do 
+    echo "Copying file - $filename";
+done
+
+
+
+
