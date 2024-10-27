922fa9ec578e11b22bd70e348be469b24eb706f7
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-10-27 18:45:36 +0000

    ARCHIVE

--- shell_scripting/disk_space.sh
+++ shell_scripting/disk_space.sh
@@ -3,7 +3,8 @@ FU=$(df -h | egrep -v "Filesystem|tmps" | grep "sda2" | awk '{print $5}' | tr -d
 
 echo $FU;
 
+TO="xyz@gmail.com";
 if [[ $FU -ge 80 ]];then
-     echo "WARNING! disk space is low";
+     echo "WARNING! disk space is low -$FU %" | mail -S "DISK SPACE ALERT!" $TO;
 else
       echo "Disk space is free";   
