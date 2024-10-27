35f2596540546d8bcff6a70757bbe991b86b49fe
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-10-27 19:49:38 +0000

    updated readme.md

---
+++ CPU_SCHEDULING_ALGORITHMS/test.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#define SIZE 10
+int main(){
+    printf(" 1.First come first serve \n 2.Shortest Job First \n 3.Priority Scheduling \n 4.Round-Robbin \n Shortest Remaining Job first Scheduling");
+}
