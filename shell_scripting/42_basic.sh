f57118a71891f8575c1283999c02082881ed44f9
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2024-10-27 17:35:18 +0000

    ok

---
+++ shell_scripting/42_basic.sh
@@ -0,0 +1,9 @@
+#/dev/null -> in case we don't want to print the output of a command
+# redirecting the output to >> /dev/null
+#command > /dev/null
+#cd /root &> /dev/null
+
+ping localhost.com > dev/null
+ping meta.ai > FILE2.log
+
+
