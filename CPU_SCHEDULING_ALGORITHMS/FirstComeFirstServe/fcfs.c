add1d43be2a620bdb9e1f01536ea6a7d46da7720
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2025-01-23 11:17:09 +0000

    fcfs.c

--- CPU_SCHEDULING_ALGORITHMS/FirstComeFirstServe/fcfs.c
+++ CPU_SCHEDULING_ALGORITHMS/FirstComeFirstServe/fcfs.c
@@ -6,7 +6,47 @@
 #include <stdlib.h>
 //Maximum length of the arrays
 #define SIZE 10
+typedef struct{
+    int pid;
+    int at;
+    int bt;
+    int ct;
+    int tat;
+    int wt;
+}Process;
 
+void sortByArrivalTime(Process p[],int n){
+for(int i=0;i<n;i++){
+    for(int j=0;j<n-i-1;j++){
+        if(p[j].at>p[j+1].at){
+            Process temp=p[j];
+            p[j]=p[j+1];
+            p[j+1]=temp;
+        }
+    }
+}
+}
+void Display(Process p[],int n){
+    printf("\n Process \t AT \t BT \t CT \t TAT \t WT \n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("\n %d \t %d \t %d \t %d \t %d \t %d \t \n ",p[i].pid,p[i].at,p[i].bt,p[i].ct,p[i].tat,p[i].wt);
+    }
+}
+
+void fcfs(Process p[],int n){
+    int currentTime=0;
+    for (int i1 = 0; i1 < n; i1++)
+    {
+        if(p[i1].at>currentTime){
+            currentTime=p[i1].at;
+        }
+        currentTime=currentTime+p[i1].bt;
+        p[i1].ct=currentTime;
+        p[i1].tat=p[i1].ct-p[i1].at;
+        p[i1].wt=p[i1].tat-p[i1].bt;
+    }
+}
 int main() {
    //Variables 
    //at[SIZE] -> arrival time
@@ -22,63 +62,23 @@ int main() {
    //beg->beginning
    //avg->sum total of waiting time
    //totalt-> total time
-    int at[SIZE], bt[SIZE], bt_copy[SIZE];
-    int wt[SIZE], tat[SIZE], completion[SIZE];
-    int i, count = 0, timecnt = 0, n, beg = 0;
-    float avg = 0, totalt = 0;
+    // int at[SIZE], bt[SIZE], bt_copy[SIZE];
+    // int wt[SIZE], tat[SIZE], completion[SIZE];
+    // int i, count = 0, timecnt = 0, n, beg = 0;
+    // float avg = 0, totalt = 0;
     
-    
-    printf("Enter the number of processes (Maximum %d):", SIZE - 2);
+    int n;
+    printf("Enter the number of processes ");
     scanf("%d", &n);
-
-    // Input details of processes
-    for (i = 0; i < n; i++) {
-        printf("Enter Details of process %d \n", i);
-        printf("Arrival Time:");
-        scanf("%d", &at[i]);
-        printf("Burst Time: ");
-        scanf("%d", &bt[i]);
-        bt_copy[i] = bt[i];  // copy burst time for later use
-    }
-
-    printf("\n Gantt Chart for FCFS scheduling \n :");
-    printf("time start to end => process number \n");
-   // Criteria: Arrival Time
-   //time count is initially zero from where gantt chart starts, we have to iterate from zeroth proccess and count should not be equal to the total number of processes 
-    for (timecnt = 0, i = 0; count != n;) {
-        // Define the conditions
-        //arrival time should be less than or equal to time count burst time of the process should be greater than zero
-        if (at[i] <= timecnt && bt[i] > 0) {
-            timecnt = timecnt + bt[i]; //add the burst time to the time count
-            bt[i] = 0; // resetting burst time for the next process
-            printf("%2d to %2d => p %d \n", beg, timecnt, i);
-            beg = timecnt;//beggining point of the next process should pe the last calculated time count 
-            count++;//counter
-            completion[i] = timecnt; //completion time
-            tat[i] = completion[i] - at[i]; // initialize tat
-            wt[i] = tat[i] - bt_copy[i];    // calculate wt
-            i = 0;
-        } else {
-            i++;
-            if (i == n) {  // reset i to 0 if this exceeds n
-                i = 0;
-                timecnt++;
-            }
-        }
-    }
-
-    printf("pid arrival burst completion turnaround waiting");
-    for (i = 0; i < n; i++) {
-        printf("\n P%d %6d %7d %9d %11d %10d", i, at[i], bt_copy[i], completion[i], tat[i], wt[i]);
-        avg = avg + wt[i];
-        totalt = totalt + tat[i];
-    }
-
-    printf("\n\n Total Waiting Time = %6.3f", avg);
-    printf("\n Total Turnaround time = %6.3f", totalt);
-    printf("\n Average Waiting Time = %6.3f", avg / n);
-    printf("\n Average Turnaround Time = %6.3f", totalt / n);
-
-    return 0;
+    Process processes[n];
+      for(int i=0;i<n;i++){
+        processes[i].pid=i+1;
+        printf("Enter the arrival time and burst time of Process %d",i+1);
+        scanf("%d %d",&processes[i].at,&processes[i].bt);
+      }
+     sortByArrivalTime(processes,n);
+     fcfs(processes,n);
+     Display(processes,n);
+     return 0;
 }
 
