d593e85ad3a9b4ea6d238b77f2a973a8dc162594
2004_Aniket_Datta <116451512+2004mathematicsEnthusiastAniketDatta@users.noreply.github.com>
2025-01-23 19:11:33 +0000

    priority_scheduling_non_preemptive

--- CPU_SCHEDULING_ALGORITHMS/Priority_Scheduling/priorityScheduling_nonpreemptive.c
+++ CPU_SCHEDULING_ALGORITHMS/Priority_Scheduling/priorityScheduling_nonpreemptive.c
@@ -1,63 +1,87 @@
-/* Priority (preemtive) CPU Scheduling Program in C */
 #include <stdio.h>
-#define SIZE 10
-
-int main()
-{
-    int at[SIZE],bt[SIZE],bt_copy[SIZE],priority[SIZE];
-    int wt[SIZE],tat[SIZE],completion[SIZE];
-    int i,count=0,timecnt,n,beg=0,temp,smallest;
-    float avg=0,totalt=0;
- 
-    printf("lowest numerical value is given the highest priority\n");
-    printf("Enter the number of Processes(Maximum %d): ", SIZE-2);
-    scanf("%d",&n);
-    //Input details of processes
-    for(i = 0; i < n; i++){
-        printf("Enter Details of Process %d \n", i);
-        printf("priority value(min 0): ");
-        scanf("%d", &priority[i]);
-        printf("Arrival Time: ");
-        scanf("%d", &at[i]);
-        printf("Burst Time  : ");
-        scanf("%d", &bt[i]);
-        bt_copy[i] = bt[i];
-    }
-    priority[SIZE-1]=9999;
-    temp = SIZE-1;
-    printf("\nGantt Chart Priority (preemtive) Scheduling\n");
-    printf("time start to end => process number\n");
-    for(timecnt=0; count!=n; timecnt++){
-        // define the conditions
-        smallest=SIZE-1;
-        for(i=0; i<n; i++){
-            if(at[i]<=timecnt && bt[i]>0 && priority[i]<priority[smallest]){
-                smallest=i;
+typedef struct{
+    int pid,at,bt,ct,tat,wt,priority,completed;
+}Process;
+void sortByArrivalTime(Process p[],int n){
+    for(int i=0;i<n;i++){
+        for (int j=0;j<n-i-1;j++){
+            if(p[j].at>p[j+1].at){
+                Process temp=p[j];
+                p[j]=p[j+1];
+                p[j+1]=temp;
             }
         }
-        bt[smallest]--;
-        if (temp != smallest || bt[smallest]==0){
-            printf("%2d to %2d => p%d\n",beg,timecnt+1,smallest);
-            beg = timecnt+1;
+    }
+}
+void Display(Process p[],int n){
+    float avg_tat=0,avg_wt=0;
+    printf("\n+----------+----------+----------+----------+----------+----------+----------+");
+    printf("\n| Process  |    AT    |    BT    | Priority |    CT    |   TAT    |    WT    |");
+    printf("\n+----------+----------+----------+----------+----------+----------+----------+");
+    for (int i = 0; i < n; i++)
+    {
+        printf("\n|    P%d    |    %d     |    %d     |    %d     |    %d     |    %d     |    %d     |",
+               p[i].pid,p[i].at,p[i].bt,p[i].priority,p[i].ct,p[i].tat,p[i].wt);
+        avg_tat+=p[i].tat;
+        avg_wt+=p[i].wt;
+    }
+    printf("\n+----------+----------+----------+----------+----------+----------+----------+");
+    printf("\nAverage Turnaround Time: %.2f",avg_tat/n);
+    printf("\nAverage Waiting Time: %.2f\n",avg_wt/n);
+}
+void NonPreemptivePriorityScheduling(Process p[],int n){
+    int currTime=0;
+    int CompletedProcess=0;
+    while (CompletedProcess<n)
+    {
+        int idx=-1;
+        int maxPriority=-__INT_MAX__;
+        for (int i = 0; i < n; i++)
+        {
+            if(p[i].at<=currTime && !p[i].completed){
+                if(p[i].priority>maxPriority){
+                    maxPriority=p[i].priority;
+                    idx=i;
+                }
+            }
         }
-        temp = smallest;
-        if(bt[smallest]==0){
-            count++; //decrement the process no.
-            completion[smallest] = timecnt+1;
-            tat[smallest] = completion[smallest] - at[smallest];
-            wt[smallest] = tat[smallest] - bt_copy[smallest];
+        if(idx!=-1){
+            p[idx].ct=currTime+p[idx].bt;
+            p[idx].tat=p[idx].ct-p[idx].at;
+            p[idx].wt=p[idx].tat-p[idx].bt;
+            p[idx].completed=1;
+            currTime=p[idx].ct;
+            CompletedProcess++;
+        }
+        else{
+            currTime++;
         }
     }
-    printf("pid   arrival  priority  burst  completion  turnaround  waiting");
-    for(i=0;i<n;i++){
-        printf("\n P%d %6d %9d %7d %9d %11d %10d",i, at[i], priority[i], bt_copy[i],completion[i],tat[i], wt[i]);
-        avg = avg + wt[i];
-        totalt = totalt + tat[i];
+}
+int main() {
+    int n;
+    printf("Enter the number of processes (1-10): ");
+    if (scanf("%d", &n) != 1 || n < 1 || n > 10) {
+        printf("Invalid input! Number of processes should be between 1 and 10.\n");
+        return 1;
     }
-    printf("\n\nTotal waiting time    = %6.3f",avg);
-    printf("\nTotal Turnaround time = %6.3f", totalt);
-    printf("\nAverage Waiting Time  = %6.3f", avg/n);
-    printf("\nAvg Turnaround Time   = %6.3f", totalt/n);
+
+    Process processes[n];
+    
+    for (int k = 0; k < n; k++) {
+        processes[k].pid = k + 1;
+        processes[k].completed = 0;
+        
+        printf("Enter Arrival Time , Burst Time and Priority for process P%d: ", k + 1);
+        if (scanf("%d %d %d", &processes[k].at, &processes[k].bt, &processes[k].priority) != 3) {
+            printf("Invalid input! Please enter integer values for Arrival Time, Burst Time and Priority.\n");
+            return 1;
+        }
+    }
+
+    sortByArrivalTime(processes, n);
+    NonPreemptivePriorityScheduling(processes, n);
+    Display(processes, n);
     
     return 0;
 }
